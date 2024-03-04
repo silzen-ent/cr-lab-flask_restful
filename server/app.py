3ceda6e8309b3c23cbeae1200206b36941dc84e6
Alan Silva <silzen.ent@gmail.com>
2024-03-04 11:01:41 -0700

    Completed Lab: REST Create & Retrieve Lab 2

--- server/app.py
+++ server/app.py
@@ -16,11 +16,44 @@ db.init_app(app)
 
 api = Api(app)
 
-class Plants(Resource):
-    pass
+
+class Plants(Resource): # Returns a list of all plants
+    def get(self):
+        plants_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+
+        response = make_response(
+            plants_dict_list,
+            200,
+        )
+        return response
+    
+
+    def post(self): # Adds a new plant to the DB
+        data = request.get_json() # Get the data from the request
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(new_plant.to_dict(), 201) 
+
+api.add_resource(Plants, '/plants')
+
+
 
 class PlantByID(Resource):
-    pass
+    def get(self, id): # Returns a single plant by ID
+        plants_dict = Plant.query.filter_by(id=id).first().to_dict()
+        response = make_response(
+            plants_dict,
+            200,
+        )
+        return response
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
